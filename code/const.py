c8bcc5aef6efa8c99f6b6f02f926308493c448d1
Nathalia Baptista <na.ti.baptistacosta@gmail.com>
2025-03-21 15:46:56 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the conste file

---
+++ code/const.py
@@ -0,0 +1,13 @@
+# c
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# w
+WIN_WIDTH = 576
+WIN_HEIGHT = 374
+
+# m
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCORE',
+               'EXIT')
--- code/game.py
+++ code/game.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import pygame
+
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 400))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        menu = Menu(self.window)
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # check for all events
-            #  for event in pygame.event.get():
-            #  if event.type == pygame.QUIT:
-            #       pygame.quit()
-            #         quit()  # end pygame
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
